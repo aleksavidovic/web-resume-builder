4aabab35fe7765ab2d77452f03f561ca8a80113f
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-24 05:34:16 +0200

    Mapped routes to correct templates for all xcurrent pages. Added title blocks where missing in templates that extend base/html

--- src/resume_builder/routes.py
+++ src/resume_builder/routes.py
@@ -7,14 +7,9 @@ main_bp = Blueprint(name="main", import_name=__name__)
 def index():
     return render_template("home.html")
 
-@main_bp.route("/my_resume")
-def my_resume():
-    return "<h1>My Resume</h1>"
-
-
 @main_bp.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template("about.html")
 
 @main_bp.route("/contact")
 def contact():
