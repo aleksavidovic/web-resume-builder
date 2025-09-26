c40bde3ae951707277e3dcc58d575e5662709756
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-26 02:31:21 +0200

    Basic displaying of Reesume sections

--- src/resume_builder/resume_builder_core/routes.py
+++ src/resume_builder/resume_builder_core/routes.py
@@ -1,8 +1,11 @@
 from . import resume_bp
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from ..models import BasicInfo, Summary
 
 @login_required
 @resume_bp.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("resume_core/home.html")
+    basic_info = BasicInfo.query.filter_by(user_id=current_user.id).all()
+    summaries = Summary.query.filter_by(user_id=current_user.id).all()
+    return render_template("resume_core/home.html", basic_info=basic_info, summaries=summaries)
