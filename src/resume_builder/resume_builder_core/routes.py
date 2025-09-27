2a66e1177ff3113e60c64ac475f3a88c72e22392
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-27 09:32:44 +0200

    Complete the Summary CRUD flow with forms and routes.

--- src/resume_builder/resume_builder_core/routes.py
+++ src/resume_builder/resume_builder_core/routes.py
@@ -24,27 +24,49 @@ def basic_info():
     users_basic_info = BasicInfo.query.filter_by(user_id=current_user.id).all()
     return render_template("resume_core/basic_info/basic_info.html", basic_infos=users_basic_info)
 
+########################
+## BASIC INFO: CREATE ##
+########################
 
 @login_required
-@resume_bp.route("/basic_info/<string:info_id>/delete", methods=["GET"])
-def delete_basic_info(info_id):
-    """
-    Handles deleting an existing BasicInfo entry.
-    """
-    try:
-        info_to_delete = BasicInfo.query.filter_by(
-            id=info_id, user_id=current_user.id
-        ).first()
-        if not info_to_delete:
-            raise NotFound()
-        db.session.delete(info_to_delete)
-        db.session.commit()
-        flash("Successfully deleted Basic Info entry.", "danger")
-    except Exception as e:
-        flash(f"Error while deleting Basic Info entry: {e}", "danger")
+@resume_bp.route("/basic_info/create", methods=["GET", "POST"])
+def create_basic_info():
+    form = BasicInfoForm()
+    if form.validate_on_submit():
+        print("Form submitted for validation")
+        try:
+            entry_title = form.entry_title.data
+            full_name = form.full_name.data
+            job_title = form.job_title.data
+            address = form.address.data
+            contact_email = form.contact_email.data
+            contact_phone = form.contact_phone.data
+            linkedin_url = form.linkedin_url.data
+            github_url = form.github_url.data
+            new_basic_info = BasicInfo(
+                entry_title=entry_title,
+                full_name=full_name,
+                job_title=job_title,
+                address=address,
+                contact_email=contact_email,
+                contact_phone=contact_phone,
+                linkedin_url=linkedin_url,
+                github_url=github_url,
+                user_id=current_user.id,
+            )
+            db.session.add(new_basic_info)
+            db.session.commit()
+            flash("New Basic Info Created!", "success")
+            return redirect(url_for("resume.basic_info"))
+        except Exception as e:
+            flash(f"Error while saving Basic Info: {e}")
+            return redirect(url_for("resume.create_basic_info"))
 
-    return redirect(url_for("resume.basic_info"))
+    return render_template("resume_core/basic_info/create_basic_info.html", form=form)
 
+#######################
+## BASIC INFO: EDIT  ##
+#######################
 
 @login_required
 @resume_bp.route("/basic_info/<string:info_id>/edit", methods=["GET", "POST"])
@@ -84,52 +106,45 @@ def edit_basic_info(info_id):
         "resume_core/basic_info/edit_basic_info.html", form=form, info_id=info_id
     )
 
+########################
+## BASIC INFO: DELETE ##
+########################
 
 @login_required
-@resume_bp.route("/basic_info/create", methods=["GET", "POST"])
-def create_basic_info():
-    form = BasicInfoForm()
-    if form.validate_on_submit():
-        print("Form submitted for validation")
-        try:
-            entry_title = form.entry_title.data
-            full_name = form.full_name.data
-            job_title = form.job_title.data
-            address = form.address.data
-            contact_email = form.contact_email.data
-            contact_phone = form.contact_phone.data
-            linkedin_url = form.linkedin_url.data
-            github_url = form.github_url.data
-            new_basic_info = BasicInfo(
-                entry_title=entry_title,
-                full_name=full_name,
-                job_title=job_title,
-                address=address,
-                contact_email=contact_email,
-                contact_phone=contact_phone,
-                linkedin_url=linkedin_url,
-                github_url=github_url,
-                user_id=current_user.id,
-            )
-            db.session.add(new_basic_info)
-            db.session.commit()
-            flash("New Basic Info Created!", "success")
-            return redirect(url_for("resume.basic_info"))
-        except Exception as e:
-            flash(f"Error while saving Basic Info: {e}")
-            return redirect(url_for("resume.create_basic_info"))
+@resume_bp.route("/basic_info/<string:info_id>/delete", methods=["GET"])
+def delete_basic_info(info_id):
+    """
+    Handles deleting an existing BasicInfo entry.
+    """
+    try:
+        info_to_delete = BasicInfo.query.filter_by(
+            id=info_id, user_id=current_user.id
+        ).first()
+        if not info_to_delete:
+            raise NotFound()
+        db.session.delete(info_to_delete)
+        db.session.commit()
+        flash("Successfully deleted Basic Info entry.", "danger")
+    except Exception as e:
+        flash(f"Error while deleting Basic Info entry: {e}", "danger")
 
-    return render_template("resume_core/basic_info/create_basic_info.html", form=form)
+    return redirect(url_for("resume.basic_info"))
+
+
 
 
 ######################
-######  SUMMARY ######
+###### SUMMARY #######
 ######################
 @login_required
 @resume_bp.route("/summary", methods=["GET", "POST"])
 def summary():
     summaries = Summary.query.filter_by(user_id=current_user.id).all()
     return render_template("resume_core/summary/summary.html", summaries=summaries)
+
+#####################
+## SUMMARY: CREATE ##
+#####################
 
 @login_required
 @resume_bp.route("/summary/create", methods=["GET", "POST"])
@@ -149,6 +164,73 @@ def create_summary():
             return redirect(url_for('resume.home'))   
     
     return render_template("resume_core/summary/create_summary.html", form=form)
+
+####################
+## SUMMARY: EDIT  ##
+####################
+
+@login_required
+@resume_bp.route("/summary/<string:summary_id>/edit", methods=["GET", "POST"])
+def edit_summary(summary_id):
+    """
+    Handles editing an existing BasicInfo entry.
+    """
+    try:
+        summary_id_uuid = uuid.UUID(summary_id)
+    except ValueError:
+        raise NotFound()
+
+    # Query for the specific BasicInfo object or raise a 404 error.
+    # The filter also ensures a user can only edit their own entries.
+    summary_to_edit = Summary.query.filter_by(
+        id=summary_id, user_id=current_user.id
+    ).first()
+    if not summary_to_edit:
+        raise NotFound()
+
+    form = SummaryForm(obj=summary_to_edit)
+
+    if form.validate_on_submit():
+        try:
+            # Update the object's attributes from the submitted form data
+            form.populate_obj(summary_to_edit)
+            # summary_to_edit.entry_title = form.entry_title.data
+            # summary_to_edit.content = form.content.data
+            db.session.commit()
+            flash("Your 'Summary' entry has been updated!", "success")
+        except Exception as e:
+            db.session.rollback()
+            flash(f"An error occurred while updating: {e}", "danger")
+        finally:
+            return redirect(url_for("resume.summary"))
+
+    # For a GET request, render the template with the pre-filled form
+    return render_template(
+        "resume_core/summary/edit_summary.html", form=form, summary=summary, summary_id=summary_id 
+    )
+
+#####################
+## SUMMARY: DELETE ##
+#####################
+
+@login_required
+@resume_bp.route("/summary/<string:summary_id>/delete", methods=["GET", "POST"])
+def delete_summary(summary_id):
+    try:
+        # summary_id = uuid.UUID(summary_id)
+        summary_to_delete = Summary.query.filter_by(
+            id=summary_id, user_id=current_user.id
+        ).first()
+        if not summary_to_delete:
+            raise NotFound()
+        db.session.delete(summary_to_delete)
+        db.session.commit()
+        flash("Successfully deleted Summary entry.", "danger")
+    except Exception as e:
+        flash(f"Error while deleting Summary entry: {e}", "danger")
+
+    return redirect(url_for("resume.summary"))
+
 
 
 #########################
