6e7385418934de03390e3733a5477df943274b3d
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-27 03:35:35 +0200

    Refactor timestamp fields as a mixin for models

--- src/resume_builder/models.py
+++ src/resume_builder/models.py
@@ -3,13 +3,15 @@ from flask_login import UserMixin
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+class TimeStampMixin:
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
-class User(db.Model, UserMixin):
+
+class User(db.Model, UserMixin, TimeStampMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password_hash = db.Column(db.String(300), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
     posts = db.relationship("BasicInfo", backref="user", lazy=True)
 
     def set_password(self, password):
@@ -22,7 +24,7 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}')"
 
 
-class BasicInfo(db.Model):
+class BasicInfo(db.Model, TimeStampMixin):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(50), nullable=True, default="")
     job_title = db.Column(db.String(50), nullable=True, default="")
@@ -31,23 +33,19 @@ class BasicInfo(db.Model):
     contact_phone = db.Column(db.String(30), nullable=True, default="")
     linkedin_url = db.Column(db.String(100), nullable=True, default="")
     github_url = db.Column(db.String(100), nullable=True, default="")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
-class Summary(db.Model):
+class Summary(db.Model, TimeStampMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, default="")
     content = db.Column(db.Text, nullable=True, default="")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
-class Experience(db.Model):
+class Experience(db.Model, TimeStampMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False, default="")
     job_title = db.Column(db.String(50), nullable=False, default="")
@@ -55,6 +53,5 @@ class Experience(db.Model):
     date_started = db.Column(db.Date, nullable=False, default=datetime.now(timezone.utc))
     date_finished = db.Column(db.Date, nullable=False, default=datetime.now(timezone.utc))
     descritpion = db.Column(db.Text, nullable=True, default="")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
