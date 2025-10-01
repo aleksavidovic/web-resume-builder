7a38df49c88a2f44a9ea48684b161e72503505a1
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-10-01 11:20:58 +0200

    Config updates

--- src/resume_builder/__init__.py
+++ src/resume_builder/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config["SECRET_KEY"] = os.getenv(
         "SECRET_KEY", "ASDAJSDOIASJDOAJOISADOASJDOASIDJASODJ"
     )
